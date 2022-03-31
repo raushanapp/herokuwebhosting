37d55a7716da0541bd269f8db7a17da1057f1f3e
raushan kuamr <sraushan890@gmail.com>
2022-03-31 10:27:03 +0530

    masaikey change

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const userController = require("./controller/userController")
 const {register,login} = require("./controller/authenticationController")     // login and regiter password
 const postController = require("./controller/postController");
 const cors = require("cors")
-const app = express();
+// const app = express();
 
-// console.log("expamle",app)
-app.use(cors())
-app.use(express.json());
+// // console.log("expamle",app)
+// app.use(cors())
+// app.use(express.json());
 app.use("/users",userController)
 app.use("/post",postController);
 
